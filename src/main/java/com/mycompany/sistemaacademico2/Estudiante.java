296b5ef90412d5c5299cf6f76cc7107ee0f48e2a
rcamilorodriguez1 <rcamilorodriguez@unicesar.edu.co>
2026-03-02 00:40:58 -0500

    feat: agregar encapsulamiento a clase Estudiante

--- src/main/java/com/mycompany/sistemaacademico2/Estudiante.java
+++ src/main/java/com/mycompany/sistemaacademico2/Estudiante.java
@@ -13,4 +13,45 @@ public class Estudiante {
     private String nombre;
     private String programa;
     private int semestre;
+
+    public Estudiante(String codigo, String nombre, String programa, int semestre) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.programa = programa;
+        this.semestre = semestre;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPrograma() {
+        return programa;
+    }
+
+    public void setPrograma(String programa) {
+        this.programa = programa;
+    }
+
+    public int getSemestre() {
+        return semestre;
+    }
+
+    public void setSemestre(int semestre) {
+        this.semestre = semestre;
+    }
+    
+    
 }
