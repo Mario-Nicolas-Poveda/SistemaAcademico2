85b39467c6d668970bac31ee91dd7cab68e5a97e
rcamilorodriguez1 <rcamilorodriguez@unicesar.edu.co>
2026-03-02 00:38:35 -0500

    feat: crear clase Estudiante con atributos

---
+++ src/main/java/com/mycompany/sistemaacademico2/Estudiante.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.sistemaacademico2;
+
+/**
+ *
+ * @author Jose Rodriguez
+ */
+public class Estudiante {
+    private String codigo;
+    private String nombre;
+    private String programa;
+    private int semestre;
+}
